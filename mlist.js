95e5412b6abb171cb9837e60e8b1e37b40054641
animelightnovels <cal0yzkii@gmail.com>
2016-08-02 13:30:34 +0800

    Update mlist.js

--- mlist.js
+++ mlist.js
@@ -1,1 +1,3 @@
-var oregairu1p = ["https://www.mediafire.com/","https://openload.co/","https://www.solidfiles.com"];
+      var oregairu1p = ["http://www.mediafire.com", "http://www.solidfiles.com"];
+      var oregairu2p = ["http://www.mediafire.com/2", "http://www.solidfiles.com/2", "http://www.openload.co/2"];
+      var oregairu3p = ["http://www.mediafire.com/3", "http://www.solidfiles.com/3"];
