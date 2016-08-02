7355a527e9017199d56f03561e2a2094c397956b
animelightnovels <cal0yzkii@gmail.com>
2016-08-02 15:36:07 +0800

    Delete mlist.js

--- mlist.js
+++
@@ -1,3 +0,0 @@
-      var oregairu1p = ["http://www.mediafire.com", "http://www.solidfiles.com"];
-      var oregairu2p = ["http://www.mediafire.com/2", "http://www.solidfiles.com/2", "http://www.openload.co/2"];
-      var oregairu3p = ["http://www.mediafire.com/3", "http://www.solidfiles.com/3"];
